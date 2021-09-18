5e87e6cc459b75e8cb24dc450baa4f954b4656db
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-09-18 00:07:01 -0500

    send and display message

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,7 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
   getHeroes():Observable <Hero[]>{
     const heroes =of(HEROES);
+    this.messageService.add('Heroes obtenidos');
     return heroes;
   }
 }
